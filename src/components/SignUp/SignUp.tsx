d308e0bd1fd3b1c4f086eee19c93d017e60a13ae
Alena Fatianova <ravemybody@gmail.com>
2021-05-22 01:49:17 +0500

    add h2, title for signup form

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -4,12 +4,16 @@ import './SignUp.scss'
 
 export const SignUp = () => {
     return (
-      <div className="signup_main_container"> 
+      <section className="signup_main_container"> 
         <div className="signup_form_block">
+        <div className="signup_block_head_title">
+            <h2>Войти в аккаунт</h2>
+            <span>Пожалуйста, войдите в свой аккаунт</span>
+          </div>
           <form className="form_signup">
             <Button buttonText='Click here'></Button> 
           </form>
         </div>
-      </div>
+      </section>
     )
 }
